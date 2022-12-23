f1b8d465db012ed349f3f18aebf5e083c82ec83a
Camden Van Sciver <camdenvs02@gmail.com>
2022-12-22 21:25:54 -0700

    changed how ingredients are collected from user

--- models/Recipe.js
+++ models/Recipe.js
@@ -19,12 +19,9 @@ Recipe.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        ingredient_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'ingredient',
-                key: 'id'
-            }
+        ingredients: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
         user_id: {
             type: DataTypes.INTEGER,
--- seeds/seed.js
+++ seeds/seed.js
@@ -2,10 +2,8 @@ const sequelize = require('../config/connection');
 
 const userData = require('./userData.json');
 const seedRecipe = require("./recipeData.json");
-const seedIngredient = require("./ingredientData.json");
-const recipeIngredient = require("./recipe-ingredients.json");
 
-const { User, Recipe, Ingredient, RecipeIngredient } = require('../models')
+const { User, Recipe } = require('../models')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -21,12 +19,6 @@ const seedDatabase = async () => {
       user_id: 1,
     })
   }
-
-  await Ingredient.bulkCreate(seedIngredient, {
-  })
-  
-  await RecipeIngredient.bulkCreate({recipeIngredient
-  })
 
   process.exit(0);
 };
